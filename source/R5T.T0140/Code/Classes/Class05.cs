c29d43fde01050eab76f6626efae9f9bf085c88e
davidcoats <david@getsafetycone.com>
2022-11-18 17:49:53 -0800

    Interim changes.

---
+++ source/R5T.T0140/Code/Classes/Class05.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.T0140
+{
+    public class Class05 : IInterface02
+    {
+        public string String { get; set; }
+
+        public double Double { get; set; }
+    }
+}
---
+++ source/R5T.T0140/Code/Interfaces/IInterface02.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.T0140
+{
+    public interface IInterface02
+    {
+        string String { get; set; }
+    }
+}
