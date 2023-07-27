ed0d9c4b76f59b44383c3866c7733f86d0cb5f7a
davidcoats <david@getsafetycone.com>
2023-07-27 01:32:12 -0700

    Interim changes.

---
+++ source/R5T.T0140/Code/Structs/ExampleStruct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0140
+{
+    /// <summary>
+    /// A basic struct.
+    /// </summary>
+    [UtilityTypeMarker]
+    public class ExampleStruct
+    {
+    }
+}
